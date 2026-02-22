93cb7c6c04ab1528db0ba005c34135eabe31c8e7
ShadekJaramillo <shadekjaramillo@gmail.com>
2026-02-21 21:39:49 -0500

    Added click highlighting functionality to the CountryList component

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,18 +1,37 @@
-let CountryList = ({ items }: { items: string[] }) =>
-  items.length === 0 ? (
+import type { MouseEvent } from "react";
+import { useState } from "react";
+
+let CountryList = ({ items }: { items: string[] }) => {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const handleClickEvent = (event: MouseEvent, city: string, index: number) => {
+    console.log("Clicked " + city);
+    console.log(event);
+    setSelectedIndex(index);
+  };
+
+  return items.length === 0 ? (
     <p>No item found</p>
   ) : (
     <>
-      {items.map((city) => (
-        <li className="list-group-item" key={city}>
-          {city}
+      {items.map((city, index) => (
+        <li
+          className={
+            selectedIndex === index
+              ? "list-group-item active"
+              : "list-group-item"
+          }
+          key={city}
+          onClick={(event) => handleClickEvent(event, city, index)}
+        >
+          {index + 1}. {city}
         </li>
       ))}
     </>
   );
+};
 
 function ListGroup() {
-  let items = ["NY", "TK", "SF", "LA"];
+  let items = ["New York", "Tokyo", "San Francisco", "Los Angeles"];
   // items = [];
 
   return (
