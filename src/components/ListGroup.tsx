0d60a1024f2a5e3562e1ef71ce225f57bda6f068
ShadekJaramillo <shadekjaramillo@gmail.com>
2026-02-22 00:21:05 -0500

    Now the 'ItemsList' component accepts a 'onSelect' trigger and a 'heading' parameter

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,7 +1,13 @@
 import type { MouseEvent } from "react";
 import { useState } from "react";
 
-let CountryList = ({ items }: { items: string[] }) => {
+interface CountryList {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+let ItemsList = ({ heading, items, onSelectItem }: CountryList) => {
   const [selectedIndex, setSelectedIndex] = useState(-1);
   const handleClickEvent = (event: MouseEvent, city: string, index: number) => {
     console.log("Clicked " + city);
@@ -9,39 +15,48 @@ let CountryList = ({ items }: { items: string[] }) => {
     setSelectedIndex(index);
   };
 
-  return items.length === 0 ? (
-    <p>No item found</p>
-  ) : (
+  let list =
+    items.length === 0 ? (
+      <p>No item found</p>
+    ) : (
+      <ul className="list-group">
+        {items.map((city, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={city}
+            onClick={(event) => {
+              handleClickEvent(event, city, index);
+              onSelectItem(city);
+            }}
+          >
+            {index + 1}. {city}
+          </li>
+        ))}
+      </ul>
+    );
+
+  return (
     <>
-      {items.map((city, index) => (
-        <li
-          className={
-            selectedIndex === index
-              ? "list-group-item active"
-              : "list-group-item"
-          }
-          key={city}
-          onClick={(event) => handleClickEvent(event, city, index)}
-        >
-          {index + 1}. {city}
-        </li>
-      ))}
+      <h1>{heading}</h1>
+      {list}
     </>
   );
 };
 
 function ListGroup() {
-  let items = ["New York", "Tokyo", "San Francisco", "Los Angeles"];
+  let items = ["New York", "Tokyo", "San Francisco", "Los Angeles", "El paso"];
   // items = [];
-
+  const handleSelectItem = (item: string) => {
+    console.log(item + " handler activated");
+  };
   return (
-    <>
-      <h1>Title changed</h1>
-      <ul className="list-group">
-        <CountryList items={items} />
-      </ul>
-    </>
+    <ItemsList items={items} heading="cities" onSelectItem={handleSelectItem} />
   );
 }
 
 export { ListGroup };
+export { ItemsList };
