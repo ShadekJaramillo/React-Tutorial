3185e0593b0024214b5507ab6fd36a662b12b89b
ShadekJaramillo <shadekjaramillo@gmail.com>
2026-02-21 19:06:05 -0500

    Added the country list in the form of a 'formal' component

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,24 +1,26 @@
+let CountryList = ({ items }: { items: string[] }) =>
+  items.length === 0 ? (
+    <p>No item found</p>
+  ) : (
+    <>
+      {items.map((city) => (
+        <li className="list-group-item" key={city}>
+          {city}
+        </li>
+      ))}
+    </>
+  );
+
 function ListGroup() {
   let items = ["NY", "TK", "SF", "LA"];
-  items = [];
-
-  let message =
-    items.length === 0 ? (
-      <p>No item found</p>
-    ) : (
-      <>
-        {items.map((city) => (
-          <li className="list-group-item" key={city}>
-            {city}
-          </li>
-        ))}
-      </>
-    );
+  // items = [];
 
   return (
     <>
       <h1>Title changed</h1>
-      <ul className="list-group">{message}</ul>
+      <ul className="list-group">
+        <CountryList items={items} />
+      </ul>
     </>
   );
 }
